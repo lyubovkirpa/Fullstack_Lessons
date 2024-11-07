ef1ea883284194e377125e556f50726cc3fe64a3
unknown <kirpa.lyubov.a@gmail.com>
2024-11-07 10:43:42 +0100

    init

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Lesson5 from "./lessons/lesson5/Lesson5";
+// import Lesson1 from "./lessons/lesson1/Lesson1";
+// import Lesson2 from "./lessons/lesson2/Lesson2";
+// import Lesson3 from "./lessons/lesson3/Lesson3";
+// import Lesson04 from "./lessons/lesson4/Lesson04";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    {/* <Lesson1 />
+    <Lesson2 />
+    <Lesson3 /> 
+     <Lesson04/> */}
+    <Lesson5/>
+  </>
+);
